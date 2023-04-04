f79cc6077e1c39dbcb02ce87dea3f3e6f950dae9
Nicolene Marais <nici.marais.ie@gmail.com>
2023-04-04 11:09:14 +0000

    fixed api error

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import styles from './App.module.css';
 import NavBar from './components/NavBar';
 import { Container } from 'react-bootstrap';
 import { Route, Switch } from 'react-router-dom';
+import './api/axiosDefaults';
+import SignUpForm from './pages/auth/SignUpForm';
 
 function App() {
   return (
@@ -16,7 +18,7 @@ function App() {
         <Switch>
           <Route exact path='/' render={() => <h1>Home Page</h1>}/>
           <Route exact path='/signin' render={() => <h1>Sign In</h1>}/>
-          <Route exact path='/signup' render={() => <h1>Sign Up</h1>}/>
+          <Route exact path='/signup' render={() =><SignUpForm />} />
           <Route render={()=> <p>Page Not Found! <i class="fa-solid fa-face-sad-tear"></i></p>}></Route>
         </Switch>
       </Container>
